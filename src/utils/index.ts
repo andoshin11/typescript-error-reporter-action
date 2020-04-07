4e47672364219b6cc7ee441abf3e7b0d4239d605
andoshin11 <shinglish11@gmail.com>
2020-04-07 19:46:16 +0900

    Load default lib files depending on the target

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,6 +5,8 @@ export function nonNullable<T>(arg: T): arg is NonNullable<T> {
   return arg !== undefined || arg !== null
 }
 
+export const uniq = <T>(arr: T[]) => arr.filter((elm, i, self) => self.indexOf(elm) === i)
+
 export function getAllLibs(libs: string[]) {
   const allLibs: { [name: string]: boolean } = {}
 
