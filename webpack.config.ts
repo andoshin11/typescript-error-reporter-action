51b148277c8bafa44b673277b412e98187769daf
Evan Jacobs <probablyup@gmail.com>
2021-01-14 22:32:59 -0500

    ts 4 & webpack 5

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import webpack from 'webpack';
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+} as webpack.Configuration;
